3efdca2dce3776ccc57f55c65c97ef3eefa063a8
qgi <qgi@offliners.com>
2014-04-21 15:37:50 +0200

    - Removed code for visual element - ActionBar is shown when the quickslot is changed

--- qbar.lua
+++ qbar.lua
@@ -1,14 +1,11 @@
 local N_SLOTS = 8
-local DURATION = 250
-local texture = ""
-local timeline = nil
 local EMPTY_SLOT_SOUND = "Ability_WrongWeapon"
 
 QBAR = {}
 
 for i=1, N_SLOTS do
    ZO_CreateStringId("SI_BINDING_NAME_QBAR_SLOT_"..i,
-		     "Quickbar Slot "..i)
+		     "Select Quickbar Slot "..i)
 end
 
 local function getSlot(n)
@@ -19,32 +16,6 @@ local function getSlot(n)
    end
 end
 
-local function useItemAtSlot(slot)
-   for bagId=1, GetMaxBags() do
-      _, nSlots = GetBagInfo(bagId)
-      for slotIndex=1, nSlots do
-	 if GetItemCurrentActionBarSlot(bagId, slotIndex) == slot then
-	    d(GetItemName(bagId, slotIndex))
-	    CallSecureProtected("UseItem", bagId, slotndex)
-	    return
-	 end
-      end
-   end
-end
-
-local function animate()
-   if not timeline then
-      timeline = ANIMATION_MANAGER:CreateTimeline()
-      timeline:SetPlaybackType(0, 0)
-      animation = timeline:InsertAnimation(ANIMATION_ALPHA, QBARTexture, 0)
-      animation:SetDuration(DURATION)
-      animation:SetAlphaValues(0, 1)
-      animation:SetEasingFunction(ZO_LinearEase)
-   end
-   timeline:PlayFromStart()
-   timeline:PlayFromEnd()
-end
-
 function QBAR.SlotKeyDown(n)
    slot = getSlot(n)
    if GetSlotBoundId(slot) == 0 or GetSlotItemCount(slot) == 0 then
@@ -53,14 +24,14 @@ function QBAR.SlotKeyDown(n)
       SetCurrentQuickslot(slot)
       itemSoundCategory = GetSlotItemSound(slot)
       PlayItemSound(itemSoundCategory, 1)
-      texture, _, _ = GetSlotTexture(slot)
-      QBARTexture:SetTexture(texture)
-      animate()
+      if ZO_ActionBar1:IsHidden() then
+	 ZO_ActionBar1:ToggleHidden()
+      end
+      if ZO_ActionBar1:GetAlpha() == 0 then
+	 ZO_ActionBar1:SetAlpha(1)
+	 -- TODO: Should fade out after some time
+      End
    end
 end
-
-function QBAR.Update()
-end
-
 
 
